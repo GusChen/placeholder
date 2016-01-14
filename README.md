141f0c632c1a97f271a5fb7d26a07d9147f0fa0b
GusChen <guanxun_chen@foxmail.com>
2016-01-14 11:28:44 +0800

    Rewrite

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# placeholder
+# placeholder v0.2
 
 兼容ie的placeholder小插件
 
@@ -15,24 +15,14 @@
 
 	```js
 	<script>
-	//第一种方式
-	new Placeholder({selecter:".cla1",text:"文字1",color:"#d3d3d3"});
 	//第二种方式
-	Placeholder2.add({selecter:".cla2",text:"文字2",color:"#d3d3d3"});
-	//上面的内容中
-	//selecter 为一个选择器，可以是一个class也可以是一个id
-	//text 是提示的文字
-	//color 是可选项 提示文字的颜色
+	Placeholder(); //这样就可以了 做了自适应
 	</script>
 	```
 
 	html代码
 
 	```html
-	<input class="cla1" type="text" />
+	<input type="text" placeholder="test"/>
 	```
 
-## 作用
-
-在html5页面中可以使用placeholder，但是如果页面同时要兼容ie浏览器的时候该属性是无效的，所以写了这个插件来兼容这个问题。
-第一次写js插件，希望有人能给点意见
--- placeholder.js
+++ placeholder.js
@@ -1,118 +1,53 @@
 /*****
- *添加格式 new Placeholder({selecter:".class",text:"文字",color:"color"});
+ * @author Guschen
+ * @github https://github.com/Guschen/placeholder
+ * @modified 2016/1/14
  **/
+!(function(window, $){
+    function Placeholder(){
+        return Placeholder.prototype.init();
+    }
 
-var Placeholder = function (obj) {
-    //选择器
-    this.selecter = obj.selecter;
-    //文字
-    this.text = obj.text;
-    //颜色
-    this.color = obj.color || "#C5C5C5";
-    //启动函数
-    this.init();
-}
-
-Placeholder.prototype.init = function () {
-    if (this.isPlaceholderSupport()) {
-        this.support();
-    } else{
-        this.nonsupport();
-    };
-}
-
-Placeholder.prototype.isPlaceholderSupport = function () {
-    //判断是否支持placeholder
-    return 'placeholder' in document.createElement('input');  
-}
-
-//支持placeholder
-Placeholder.prototype.support = function () {
-    $(this.selecter).attr("placeholder",this.text);
-}
-
-//不支持placeholder
-Placeholder.prototype.nonsupport = function () {
-    var T = this.selecter;
-    var Text = this.text;
-    var color = this.color;
-    //初始化
-    $(T).val(Text);
-    $(T).css("color", color);
-    //focus 聚焦
-    $(T).focus(function () {
-        if ($(this).val() == Text) {
-            $(this).css("color", '');
-            $(this).val("");
-        }
-    });
-    //离开
-    $(T).blur(function(){
-        if ($(this).val() == '') {
-            $(this).css("color", color);
-            $(this).val(Text);
-        }
-    });
-}
-
-
-
-var Placeholder2 = Placeholder2 || {};
-Placeholder2 = function () {
-    //"use strict"
-    var Placeholder2 = {};
-    var Pl = {};
-
-    //unil
-    function _init() {
-        if (isPlaceholderSupport()) {
+    //init 方法
+    Placeholder.prototype.init = function (){
+        if (!isPlaceholderSupport()) {
             support();
-        } else {
-            nonsupport();
         }
     }
-    function support() {
-        $(Pl.selecter).attr("placeholder",Pl.text);
-    }
 
-    //不支持placeholder
-    function nonsupport() {
-        var T = Pl.selecter;
-        var Text = Pl.text;
-        var color = Pl.color;
-        //初始化
-        $(T).val(Text);
-        $(T).css("color", color);
-        //focus 聚焦
-        $(T).focus(function () {
-            if ($(this).val() == Text) {
-                $(this).css("color", '');
-                $(this).val("");
-            }
-        });
-        //离开
-        $(T).blur(function(){
-            if ($(this).val() == '') {
-                $(this).css("color", color);
-                $(this).val(Text);
-            }
-        });
-    }
-    
     //是否支持placeholder
     function isPlaceholderSupport() {  
         return 'placeholder' in document.createElement('input');  
-    } 
-
-    //添加
-    Placeholder2.add = function (obj) {  
-        Pl.selecter = obj.selecter;
-        //文字
-        Pl.text = obj.text;
-        //颜色
-        Pl.color = obj.color || "#C5C5C5";
-        //init
-        _init();
     }
-    return Placeholder2;
-} ();
+
+    //添加支持
+    function support(){
+        $("input[type='text'], textarea").each(function(){
+            if (typeof($(this).attr("placeholder"))!="undefined") {
+                inputPlaceholder(this);
+            }
+        });
+    }
+
+    //input
+    function inputPlaceholder(_t){
+        var color = "#c5c5c5"; //(ノ=Д=)ノ┻━┻
+        var _val = $(_t).attr("placeholder");
+        $(_t).val(_val);
+        $(_t).css("color", color);
+        $(_t).focus(function () {
+            if ($(_t).val() == _val) {
+                $(_t).css("color", '');
+                $(_t).val("");
+            }
+        });
+        $(_t).blur(function(){
+            if ($(_t).val() == '') {
+                $(_t).css("color", color);
+                $(_t).val(_val);
+            }
+        });
+    }
+    Placeholder.prototype.init.prototype = Placeholder.prototype;
+    window.Placeholder = Placeholder;
+}(window,$));
